e5ed994353bb99de46906f20d8cbb4c171f1f037
jinwoo <upbreak05@kakao.com>
2024-03-06 17:30:21 +0900

    project setup && add appspec.yml

---
+++ src/main/java/com/jinwoo/dev/githubactionfcamdev/GithubActionFcamDevApplication.java
@@ -0,0 +1,13 @@
+package com.jinwoo.dev.githubactionfcamdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionFcamDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubActionFcamDevApplication.class, args);
+    }
+
+}
