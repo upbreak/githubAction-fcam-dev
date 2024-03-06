98553f983e2351667dcdfad9120504434f5f5dbf
jinwoo <upbreak05@kakao.com>
2024-03-06 18:52:31 +0900

    add test demoController

---
+++ src/main/java/com/jinwoo/dev/githubactionfcamdev/controller/DemoController.java
@@ -0,0 +1,13 @@
+package com.jinwoo.dev.githubactionfcamdev.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/demo")
+    public String demo (){
+        return "demo";
+    }
+}
